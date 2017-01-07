5e1c4a1647d0d2207e356510104328ff1cacfe10
William Turner <willtur.will@gmail.com>
2017-01-07 09:03:53 -0500

    Changes PFaaS to use game

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import requests
 from random import choice
 from game import Game
 
-def path_find(state, start, target):
+def path_find(game, start, target):
     try:
         r = requests.get('http://game.blitz.codes:8081/pathfinding/direction',
-                         params={'map': state['game']['board']['tiles'],
-                                 'size': state['game']['board']['size'],
+                         params={'map': game.state['game']['board']['tiles'],
+                                 'size': game.state['game']['board']['size'],
                                  'start': '(%d,%d)' % start,
                                  'target': '(%d,%d)' % target})
         json = r.json()
